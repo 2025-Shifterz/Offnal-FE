f56ed8eb9ae030f498797efea4168cb33d2a8262
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2025-06-17 22:52:50 +0900

    [chore][KanuKim97] Update tailwind.config.js
    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  
+  darkMode: false,
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#657786',
+        background: '#E1E8ED',
+        text: '#14171A',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+    },
   },
   plugins: [],
+  corePlugins: require('tailwindcss-react-native/unsupported-core-plugins'),
 }
