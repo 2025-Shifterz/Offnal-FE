5b999e0d7dcda6264f6806a73faf882a17ac6932
geonwoo.kim <angry_ryan@kakao.com>
2025-06-21 22:38:37 +0900

    [chore][KanuKim97] reanimated 설치
    [chore][KanuKim97] reanimated 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
@@ -12,4 +15,3 @@ module.exports = {
   },
   plugins: [],
 }
-
