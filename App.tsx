bf9456901f94c2888d153a688f5f373d4548d013
rngrhn4114 <is9206855@gmail.com>
2025-06-27 03:48:44 +0900

    [feat][rngrhn4114] Design System Apply

--- App.tsx
+++ App.tsx
@@ -1,23 +1,19 @@
-import "./global.css"
+import './global.css';
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-
-        <SafeAreaView className="flex-1 items-center justify-center bg-white">
-          <Text className="text-2xl font-bold text-primary">
-            Hello, Nativewind!
-          </Text>
-          <Text className="text-xl font-bold text-secondary">
-            Welcome to Nativewind!
-            </Text>
-        </SafeAreaView>
-      </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <SafeAreaView className="flex-1 items-center justify-center bg-white">
+        <Text className="font-bold text-alpha-black0">Hello, Nativewind!</Text>
+        <Text className="text-warning-60">Welcome to Nativewind!</Text>
+        <Text className="font-2xl text-danger-70">짜증나</Text>
+        <Text className="text-danger-70 text-lg font-bold">
+          스타일 적용되면 이 문구 보일 거야
+        </Text>
+      </SafeAreaView>
+    </View>
   );
 }
