29265cfae0add0a623aa819dcda5565f0da7e1be
rngrhn4114 <is9206855@gmail.com>
2025-06-27 19:39:44 +0900

    Update tailwind.config.js & font 설정

--- App.tsx
+++ App.tsx
@@ -7,12 +7,8 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <SafeAreaView className="flex-1 items-center justify-center bg-white">
-        <Text className="font-bold text-alpha-black0">Hello, Nativewind!</Text>
+        <Text className="text-success-60">Hello, Nativewind!</Text>
         <Text className="text-warning-60">Welcome to Nativewind!</Text>
-        <Text className="font-2xl text-danger-70">짜증나</Text>
-        <Text className="text-danger-70 text-lg font-bold">
-          스타일 적용되면 이 문구 보일 거야
-        </Text>
       </SafeAreaView>
     </View>
   );
