c2485c347331178e523e0d8c474d7916937d731b
sophia22001 <yujii22001@gmail.com>
2025-06-29 23:10:29 +0900

    [chore][sophia22001] eslint, prettier 설정

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
-import "./global.css"
+import './global.css';
 import React from 'react';
-import {
-  Text,
-  View,
-} from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-      <View className="flex-1 items-center justify-center bg-white">
-
-        <SafeAreaView className="flex-1 items-center justify-center bg-white">
-          <Text className="text-2xl font-bold text-primary">
-            Hello, Nativewind!
-          </Text>
-          <Text className="text-xl font-bold text-secondary">
-            Welcome to Nativewind!
-            </Text>
-        </SafeAreaView>
-      </View>
+    <View className="flex-1 items-center justify-center bg-white">
+      <SafeAreaView className="flex-1 items-center justify-center bg-white">
+        <Text className="text-2xl font-bold text-primary">Hello, Nativewind!</Text>
+        <Text className="text-xl font-bold text-secondary">Welcome to Nativewind!</Text>
+      </SafeAreaView>
+    </View>
   );
 }
