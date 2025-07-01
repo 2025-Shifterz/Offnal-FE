c7dc2fc2c017f3750d434db4595e8a0ad4578f9b
rngrhn4114 <is9206855@gmail.com>
2025-07-01 19:05:26 +0900

    [fix][rngrhn4114]: 디자인 시스템 최종 & 다크/라이트 모드 테스트용 코드 추가

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
 import './global.css';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
+import {colorScheme} from 'nativewind';
+import {useColorScheme} from 'react-native';
+
 export default function App() {
+  const scheme = useColorScheme(); // 'light' or 'dark'
+
+  useEffect(() => {
+    if (scheme === 'light' || scheme === 'dark') {
+      colorScheme.set('dark');
+    }
+  }, [scheme]);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <SafeAreaView className="flex-1 items-center justify-center bg-white">
-        <Text className="text-success-60 text-heading-xl font-pretendard">
+    <View className="flex-1 items-center justify-center bg-white ">
+      <SafeAreaView className="flex-1 items-center justify-center bg-white ">
+        <Text className="dark:text-dark-action-primary text-success-80 dark:bg-slate-500 text-heading-xl font-pretendard">
           Hello, Nativewind!
         </Text>
         <Text className="text-warning-60 text-heading-xl font-pretendard">
