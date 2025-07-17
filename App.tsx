1c110c45cda920be76945e453ade9b49ed9a15d1
geonwoo.kim <angry_ryan@kakao.com>
2025-07-17 17:44:43 +0900

    Squashed commit of the following:
    commit aeaf13bf69d26b17acab224dda9876c1fb62d9f9
    Merge: b21f120 5af875f
    Author: yujin <127318207+sophia22001@users.noreply.github.com>
    Date:   Thu Jul 17 01:00:54 2025 +0900
    
        Merge pull request #11 from 2025-Shifterz/feat/schedule-api
    
        [PR]: [feat] 근무표 UI 와 API 코드 작성
    
    commit 5af875fbf16742e8a3fe77102f838e471ef39c6f
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Thu Jul 17 00:47:52 2025 +0900
    
        [feat][sophia22001]: 다중 월 데이터 반영
    
    commit 9374920cf19a6de0393a515db8da781085b42eb4
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Thu Jul 17 00:07:49 2025 +0900
    
        [feat][sophia22001]: podfile 재설치
    
    commit 38d41f044eb35d1ed461e60b8f23f21dd7e9ae9f
    Merge: e3b24f0 b21f120
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Thu Jul 17 00:07:22 2025 +0900
    
        Merge branch 'dev' into feat/schedule-api
    
    commit e3b24f02c542849fcaad60212e120e66aae34079
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Wed Jul 16 23:42:12 2025 +0900
    
        [feat][sophia22001]: todo UI
    
    commit b88b6945cdb2df2d7c914756d7e2cbf163e27697
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Wed Jul 16 16:36:15 2025 +0900
    
        [feat][sophia22001]: 근무표 등록, 조회 API
    
    commit f6035c83f75c96e21e37b8ab13cca857a4727ee8
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Wed Jul 16 14:17:03 2025 +0900
    
        [feat][sophia22001]: 팀 캘린더 기능 완성
    
    commit 6f84e56f3e05e48c3489e65ef6a4b4adb9e1725f
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Tue Jul 15 23:13:18 2025 +0900
    
        [feat][sophia22001]: 팀 캘린더 UI
    
    commit d6941dd44e2c3302c9f375197de0ac8eb8054b29
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Tue Jul 15 19:38:55 2025 +0900
    
        [feat][sophia22001]: 캘린더 직접 구현
    
    commit faa902a3911509b7781e928eba83f306457f30ff
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Mon Jul 14 19:44:50 2025 +0900
    
        [feat][sophia22001]: 캘린더 탭 화면 조정
    
    commit 493c8d24220fcb8a6af67c91daa4b84af4acc89d
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Mon Jul 14 19:44:19 2025 +0900
    
        [feat][sophia22001]: 캘린더 월 이동 조절
    
    commit 91daded350fc113fbfbf602bd7c1f41794e492a7
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Mon Jul 14 19:43:17 2025 +0900
    
        [feat][sophia22001]: params 전달
    
    commit 286eec35894ea52508ef7e03880ac50edd1f3b36
    Author: sophia22001 <yujii22001@gmail.com>
    Date:   Mon Jul 14 17:53:43 2025 +0900
    
        [feat][sophia22001]: 근무표 API 요청

--- App.tsx
+++ App.tsx
@@ -6,14 +6,41 @@ import AppNavigator from './src/navigation/AppNavigator';
 import { colorScheme } from 'nativewind';
 import { useColorScheme } from 'react-native';
 
+import BottomNavigationBar, { Tab } from './src/presentation/main/components/BottomNavigationBar';
+
 import { enableScreens } from 'react-native-screens';
-import MyInfoScreen from './src/presentation/myInfo/screen/MyInfoScreen';
-import PrivacyPolicesScreen from './src/presentation/myInfo/screen/PrivacyPolicesScreen';
-import TermsOfUseScreen from './src/presentation/myInfo/screen/TermsOfUseScreen';
-import UpdateMyInfoScreen from './src/presentation/myInfo/screen/UpdateMyInfoScreen';
-
+import RegisterScheduleScreen from './src/presentation/onboarding/screen/RegisterScheduleScreen';
 enableScreens();
+import { Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// const Stack = createNativeStackNavigator();
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CompleteCreate from './src/presentation/completeCreate/screen/CompleteCreate';
+import CustomBackButton from './src/presentation/common/component/CustomBackButton';
+import StepBar from './src/presentation/common/component/StepBar';
+import TodoScreen from './src/presentation/note/screen/TodoScreen';
+import MemoScreen from './src/presentation/note/screen/MemoScreen';
+import MainScreen from './src/presentation/main/screen/MainScreen';
+import CalendarScreen from './src/presentation/calendar/screen/CalendarScreen';
+import MyInfoScreen from './src/presentation/myInfo/screen/MyInfoScreen';
+import ScheduleRegType from './src/presentation/scheduleRegType/screen/ScheduleRegType';
+import ScheduleInfoInput from './src/presentation/scheduleInpInput/screen/ScheduleInfoInput';
+import CalendarType from './src/presentation/calenderType/screen/CalendarType';
+
+const Stack = createNativeStackNavigator();
+
+function TabNavigator() {
+  return (
+    <BottomNavigationBar>
+      <Tab.Screen name="Home" component={MainScreen} />
+      <Tab.Screen name="Calendar" component={CalendarScreen} />
+      <Tab.Screen name="MyInfo" component={MyInfoScreen} />
+    </BottomNavigationBar>
+  );
+}
 
 export default function App() {
   const scheme = useColorScheme();
@@ -26,10 +53,47 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <MyInfoScreen/>
-      {/* <TermsOfUseScreen/> */}
-      {/* <PrivacyPolicesScreen/> */}
-      {/* <UpdateMyInfoScreen/> */}
+      {/* <AppNavigator /> */}
+      {/* <RegisterScheduleScreen/> */}
+
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShadowVisible: false,
+            headerStyle: { backgroundColor: '#F4F5F6' },
+            headerLeft: () => <CustomBackButton />,
+            headerTitleAlign: 'center',
+          }}
+        >
+          {/* 탭 네비게이션은 루트에서 보여줍니다 */}
+          <Stack.Screen name="MainTabs" component={TabNavigator} options={{ headerShown: false }} />
+
+          <Stack.Screen name="Todo" options={{ title: '할 일' }} component={TodoScreen} />
+          <Stack.Screen name="Memo" options={{ title: '메모' }} component={MemoScreen} />
+
+          {/* 스택으로 push 되는 화면들 */}
+          <Stack.Screen
+            name="ScheduleRegType"
+            component={ScheduleRegType}
+            options={{ headerTitle: () => <StepBar currentStep={0} totalSteps={4} /> }}
+          />
+          <Stack.Screen
+            name="ScheduleInfoInput"
+            component={ScheduleInfoInput}
+            options={{ headerTitle: () => <StepBar currentStep={1} totalSteps={4} /> }}
+          />
+          <Stack.Screen
+            name="CalendarType"
+            component={CalendarType}
+            options={{ headerTitle: () => <StepBar currentStep={2} totalSteps={4} /> }}
+          />
+          <Stack.Screen
+            name="CompleteCreate"
+            component={CompleteCreate}
+            options={{ headerTitle: () => <StepBar currentStep={3} totalSteps={4} /> }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
