4cb5ee13ce345326337d4375e05b37a068d5b41b
geonwoo.kim <angry_ryan@kakao.com>
2025-07-19 04:40:10 +0900

    [feat][KanuKim97] Splash Screen update
    Splash Screen 구현

--- src/presentation/splash/screen/SplashScreen.tsx
+++ src/presentation/splash/screen/SplashScreen.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { RootStackParamList } from '../../../navigation/types';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import LinearGradient from 'react-native-linear-gradient';
+import SplashLogo from '../../../assets/icons/ic_splash_logo.svg';
 
 // import AppLogo from '../../assets/icons/app_logo.svg'; // 사용 예정(아마 사용 안할듯)
 // import LottieSplash from '../../assets/lottie/splash.json'; // 사용 예정
@@ -29,16 +32,38 @@ const SplashScreen = () => {
     checkTokenAndNavigate();
   }, [navigation]);
 
+  const gradientColors = [
+    'rgba(92, 192, 205, 0.1)',
+    'rgba(217, 217, 217, 0)',
+    'rgba(238, 232, 172, 0.78)',
+    'rgba(238, 232, 172, 1)',
+  ];
+
+  const gradientLocations = [0, 0.5, 0.76, 1];
+
   return (
-    <View className="flex-1 items-center justify-center bg-background-gray-subtle1">
-      <View className="bg-red-300">
-        {/* <AppLogo /> */}
-        <Text className="text-center font-pretendard text-heading-xs font-semibold leading-[1.2] tracking-letter-spacing-0 text-white">
-          스플래시 스크린
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center bg-[#5cc0cd]">
+        <LinearGradient
+          colors={gradientColors}
+          locations={gradientLocations}
+          style={styles.gradient}
+        />
+        <SplashLogo />
+
+        <Text className="mt-[18px] text-center text-heading-xs text-white">
+          더 나은 오프날을 위한{'\n'}종합 교대근무 루틴 매니저
         </Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  // StyleSheet.absoluteFill을 사용하면 부모 View를 가득 채우는 스타일이 적용됩니다.
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default SplashScreen;
