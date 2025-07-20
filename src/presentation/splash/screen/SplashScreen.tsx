b223cbb9bcf479798bde8a191cd9c0510b312831
rngrhn4114 <is9206855@gmail.com>
2025-07-21 01:52:19 +0900

    [fix][rngrhn4114] 스플래시 화면 로티 적용 및 도트 제외 스타일 적용

--- src/presentation/splash/screen/SplashScreen.tsx
+++ src/presentation/splash/screen/SplashScreen.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { RootStackParamList } from '../../../navigation/types';
 import { SafeAreaView } from 'react-native-safe-area-context';
-// import LinearGradient from 'react-native-linear-gradient';
+import LinearGradient from 'react-native-linear-gradient';
 import LottieView from 'lottie-react-native';
 
 const SplashScreen = () => {
@@ -25,45 +24,29 @@ const SplashScreen = () => {
     }
   };
 
-  const gradientColors = [
-    'rgba(92, 192, 205, 0.1)',
-    'rgba(217, 217, 217, 0)',
-    'rgba(238, 232, 172, 0.78)',
-    'rgba(238, 232, 172, 1)',
-  ];
-
-  const gradientLocations = [0, 0.5, 0.76, 1];
-
   return (
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 items-center justify-center bg-[#5cc0cd]">
-        {/* <LinearGradient
-          colors={gradientColors}
-          locations={gradientLocations}
-          style={styles.gradient}
-        /> */}
+    <SafeAreaView className="relative flex-1">
+      <LinearGradient
+        colors={['#5cc0cd', '#eee8ac']}
+        locations={[0, 1]}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      <View className="mt-[85%] flex-1 items-center">
         <LottieView
           source={require('../../../assets/lottie/SplashScreen.json')}
           autoPlay
           loop={false}
-          style={{ width: 200, height: 173 }}
           resizeMode="contain"
+          style={{ width: 250, height: 250 }}
           onAnimationFinish={handleAnimationFinish}
         />
-
-        <Text className="mt-[18px] text-center text-heading-xs text-white">
+        <Text className="mt-number-2 text-center font-pretendard text-heading-xs font-semibold leading-[1.2] tracking-letter-spacing-0 text-white">
           더 나은 오프날을 위한{'\n'}종합 교대근무 루틴 매니저
         </Text>
       </View>
     </SafeAreaView>
   );
 };
-
-// const styles = StyleSheet.create({
-//   // StyleSheet.absoluteFill을 사용하면 부모 View를 가득 채우는 스타일이 적용됩니다.
-//   gradient: {
-//     ...StyleSheet.absoluteFillObject,
-//   },
-// });
 
 export default SplashScreen;
