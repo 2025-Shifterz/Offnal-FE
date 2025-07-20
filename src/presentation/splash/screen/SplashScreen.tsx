2816ddbff82acdb7d3e0e6cc2ed64648ac47c01d
rngrhn4114 <is9206855@gmail.com>
2025-07-21 00:47:51 +0900

    [feat][rngrhn4114] 스플래시 화면 로티 추가

--- src/presentation/splash/screen/SplashScreen.tsx
+++ src/presentation/splash/screen/SplashScreen.tsx
@@ -1,36 +1,29 @@
 import { useEffect } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { RootStackParamList } from '../../../navigation/types';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import LinearGradient from 'react-native-linear-gradient';
-import SplashLogo from '../../../assets/icons/ic_splash_logo.svg';
-
-// import AppLogo from '../../assets/icons/app_logo.svg'; // 사용 예정(아마 사용 안할듯)
-// import LottieSplash from '../../assets/lottie/splash.json'; // 사용 예정
+import LottieView from 'lottie-react-native';
 
 const SplashScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
-  useEffect(() => {
-    const checkTokenAndNavigate = async () => {
-      try {
-        const token = await EncryptedStorage.getItem('accessToken');
-        const targetRoute = token ? 'Tabs' : 'LoginScreens';
+  const handleAnimationFinish = async () => {
+    try {
+      const token = await EncryptedStorage.getItem('accessToken');
+      const targetRoute = token ? 'Tabs' : 'LoginScreens';
 
-        navigation.reset({
-          index: 0,
-          routes: [{ name: targetRoute }],
-        });
-      } catch (error) {
-        console.error('Token 확인 중 오류 발생:', error);
-      }
-    };
-
-    checkTokenAndNavigate();
-  }, [navigation]);
+      navigation.reset({
+        index: 0,
+        routes: [{ name: targetRoute }],
+      });
+    } catch (error) {
+      console.error('Token 확인 중 오류 발생:', error);
+    }
+  };
 
   const gradientColors = [
     'rgba(92, 192, 205, 0.1)',
@@ -49,7 +42,14 @@ const SplashScreen = () => {
           locations={gradientLocations}
           style={styles.gradient}
         />
-        <SplashLogo />
+        <LottieView
+          source={require('../../../assets/lottie/SplashScreen.json')}
+          autoPlay
+          loop={false}
+          style={{ width: 200, height: 173 }}
+          resizeMode="contain"
+          onAnimationFinish={handleAnimationFinish}
+        />
 
         <Text className="mt-[18px] text-center text-heading-xs text-white">
           더 나은 오프날을 위한{'\n'}종합 교대근무 루틴 매니저
